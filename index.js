5cc63c53c26c98596f153fcb5327fd1d00817afa
sourav das <dass7550@gmail.com>
2023-09-25 07:39:00 +0600

    payment was working

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion,ObjectId  } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const bodyParser = require('body-parser');
 const port = 5000;
 
 //middle ware
 app.use(cors());
 app.use(express.json());
+
+
+
+const SSLCommerzPayment = require('sslcommerz-lts')
+const store_id = 'dassd650ea56590f5a'
+const store_passwd = 'dassd650ea56590f5a@ssl'
+const is_live = false
+const httpProxy = require('http-proxy');
 // app.use(bodyParser.json());
 
 /**
@@ -144,8 +152,8 @@ async function run() {
     app.post('/place-order', async (req, res) => {
       const OrderList = await client.db("dev-campus").collection("OrderList");
       const order = req.body;
-      order.approval='false'
-      order.status='Incomplete'
+      order.approval = 'false'
+      order.status = 'Incomplete'
       const result = await OrderList.insertOne(order)
       res.send(result)
     })
@@ -157,7 +165,7 @@ async function run() {
       // console.log(query.productName.toLowerCase())
       const cursor = OrderList.find(query);
       const Orders = await cursor.toArray();
-      console.log(Orders)
+      //console.log(Orders)
       res.send(Orders);
     })
     //get orders for user
@@ -178,7 +186,7 @@ async function run() {
       const objectId = new ObjectId(id);
       const updatedDocument = req.body;
       console.log(updatedDocument)
-      const ap={approval:'true'}
+      const ap = { approval: 'true' }
       try {
         const result = await OrderList.updateOne({ _id: objectId }, { $set: ap });
         res.json(result);
@@ -188,6 +196,46 @@ async function run() {
       }
     })
 
+    //----------------------------------------------------------------//
+    //----------payment system---------------------------------------//
+    app.post('/poreced-payment', async(req, res) => {
+      const PaymedCollection = await client.db("dev-campus").collection("Payments");
+      const query=(req.query)
+      const transactionID = new ObjectId().toString();
+      const data = {
+        total_amount: query.price,
+        currency: 'BDT',
+        tran_id: transactionID, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/success-payment?transactionID=${transactionID}`,
+        fail_url: `http://localhost:5000/fail?transactionID=${transactionID}`,
+        cancel_url: `http://localhost:5000/cancel`,
+        ipn_url: 'http://localhost:5000/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: query.companyEmail,
+        customerEmail:query.customerEmail
+      };
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Expose-Headers', '');
+        res.send({url:GatewayPageURL})
+        console.log('Redirecting to: ', GatewayPageURL)
+        PaymedCollection.insertOne(data)
+        // console.log('Redirecting to: ', GatewayPageURL)
+      });
+    })
+    app.post('/success-payment',(req,res)=>{
+      // console.log(req.query)
+      // query.success='True'
+      // res.redirect(`localhost:3000/payment/success?${query}`)
+    })
   } finally {
     //  jodi connencted rakhte chai taile close kora jabe na
     //  onek bishal error khaisi eitar karone. 
